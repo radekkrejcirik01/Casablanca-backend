8562a7f9935a970f8f64515753935f7bafa83c15
radekkrejcirik01 <radek.krejcirik@cloudtalk.io>
2022-12-14 20:51:52 +0100

    feat(pushnotifications): SaveDevice

--- services/pushnotifications/pkg/rest/controller/devices.go
+++ services/pushnotifications/pkg/rest/controller/devices.go
@@ -1,5 +1,32 @@
 package controller
 
+import (
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/radekkrejcirik01/Casblanca-backend/services/pushnotifications/pkg/database"
+	devices "github.com/radekkrejcirik01/Casblanca-backend/services/pushnotifications/pkg/model/devices"
+)
+
 func SaveDevice()(c *fiber.Ctx) error {
 	t := &devices.Device{}
+
+	if err := c.BodyParser(t); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(Response{
+			Status:  "error",
+			Message: err.Error(),
+		})
+	}
+
+	if err := devices.SaveDevice(database.DB, t); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(Response{
+			Status:  "error",
+			Message: err.Error(),
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(Response{
+		Status:  "succes",
+		Message: "Device succesfully perfomed",
+	})
 }
