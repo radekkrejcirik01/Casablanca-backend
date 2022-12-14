e2843085286af65d875a474a24b687099ddad0eb
radekkrejcirik01 <radek.krejcirik@cloudtalk.io>
2022-12-14 20:59:06 +0100

    feat(pushnotifications): migrate

--- services/pushnotifications/main.go
+++ services/pushnotifications/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"context"
+	"log"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	fiberadapter "github.com/awslabs/aws-lambda-go-api-proxy/fiber"
 	"github.com/radekkrejcirik01/Casblanca-backend/services/pushnotifications/pkg/database"
+	devices "github.com/radekkrejcirik01/Casblanca-backend/services/pushnotifications/pkg/model"
 	"github.com/radekkrejcirik01/Casblanca-backend/services/pushnotifications/pkg/rest"
 )
 
@@ -14,6 +16,9 @@ var fiberLambda *fiberadapter.FiberLambda
 
 func init() {
 	database.Connect()
+	if err := database.DB.AutoMigrate(&devices.Device{}); err != nil {
+		log.Fatal(err)
+	}
 
 	fiberLambda = fiberadapter.New(rest.Create())
 }
